e569844fdbf57c51b2654a57c5af77cb657941a8
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-14 16:21:26 -0500

    Adding the 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+	public static int[] CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+		int[] result = new int[size];
+		for (int x = 0; x < size; x++)
+		{
+			result[x] = x;
+		}
+		Console.WriteLine(String.Join(" ", result));
+		return result;
+	}
+}
