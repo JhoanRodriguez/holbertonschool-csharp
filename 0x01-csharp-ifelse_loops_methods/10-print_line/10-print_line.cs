16cbe9a3e29b6a700f45b833182e38b04dcd28d2
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-13 22:56:50 -0500

    Adding the 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Line
+{
+	public static void PrintLine(int length)
+	{
+		if (length <= 0)
+			Console.WriteLine();
+		else
+			Console.WriteLine(new String('_', length));
+	}
+}
