6329b8aea1ceb97caa2bedea3112079204ffb811
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-13 22:03:53 -0500

    Adding the 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (char x = 'a'; x <= 'z'; x++)
+			{
+				if (x != 'e' && x != 'q')
+				{
+					Console.Write(x);
+				}
+			}
+		}
+	}
+}
