8a1378b985e746d0c35348c6f5b5a693c0cf606c
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-13 22:17:34 -0500

    Adding the 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int x = 0; x < 100; x++)
+			{
+				if (x != 99)
+				{
+					Console.Write($"{x.ToString("00")}, ");
+				}
+				else
+				{
+					Console.Write($"{x}\n");
+				}
+			}
+		}
+	}
+}
