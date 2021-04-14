34f126f938eafa54819aa873ea8a2dd9f73eebb4
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-13 22:29:21 -0500

    Adding the 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int x = 1; x < 90; x++)
+			{
+				if (x / 10 < x % 10)
+				{
+					if (x != 89)
+						Console.Write($"{x.ToString("00")}, ");
+					else
+						Console.Write($"{x.ToString("00")}\n");
+				}
+			}
+		}
+	}
+}
