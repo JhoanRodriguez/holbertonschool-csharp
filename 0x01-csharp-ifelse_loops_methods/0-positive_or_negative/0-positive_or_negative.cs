45ce440125033d904bb87f0cafc490a6c245cb33
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-13 21:37:15 -0500

    adding the 0

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Random rndm = new Random();
+		int number = rndm.Next(-10, 10);
+		if (number > 0)
+		{
+			Console.WriteLine("{0} is positive");
+		}
+		else if (number < 0)
+		{
+			Console.WriteLine("{0} is negative");
+		}
+		else
+		{
+			Console.WriteLine("{0} is zero");
+		}
+	}
+}
