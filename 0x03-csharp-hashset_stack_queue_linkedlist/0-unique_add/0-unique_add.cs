f61b14150984e82673d512eaff917da2b3e18bcc
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-15 11:00:20 -0500

    Adding the 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		HashSet<int> setList = new HashSet<int>(myList);
+		int total = 0;
+
+		foreach (int j in setList)
+			total += j;
+
+		return total;
+	}
+}
