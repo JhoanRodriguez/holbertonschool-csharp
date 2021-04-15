d91dd80ba24ec519c6c87001fd63f418149a0eb8
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-14 22:42:15 -0500

    adding the 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[,] matrix = new int[5, 5];
+			matrix[2, 2] = 1;
+			for (int x = 0; x < 5; x++)
+			{
+				for (int y = 0; y < 5; y++)
+				{
+					if (y != 4)
+						Console.Write($"{matrix[x, y]} ");
+					else
+						Console.Write($"{matrix[x, y]}");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
