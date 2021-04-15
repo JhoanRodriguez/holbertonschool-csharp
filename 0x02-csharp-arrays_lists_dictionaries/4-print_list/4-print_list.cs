89c6fca8eb54df3cecba9b32b1c5c36b1de624ee
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-14 19:21:37 -0500

    Adding the 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+		List<int> results = new List<int>();
+		for (int x = 0; x < size; x++)
+			results.Add(x);
+		Console.WriteLine(string.Join(" ", results));
+		return results;
+	}
+}
