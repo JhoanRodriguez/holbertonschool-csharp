112ccd32b7789c5c26672ad54fa5911abdbe6545
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-14 20:04:58 -0500

    Adding the 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<bool> DivisibleBy2(List<int> myList)
+	{
+		List<bool> results = new List<bool>();
+		foreach (int x in myList)
+		{
+			if (x % 2 == 0)
+				results.Add(true);
+			else
+				results.Add(false);
+		}
+		return results;
+	}
+}
