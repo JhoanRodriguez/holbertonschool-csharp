69e60db3dda7448a140ec9480b103ccd7ffb151b
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-15 18:35:30 -0500

    Adding the 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+		LinkedList<int> newLinkedList = new LinkedList<int>();
+
+		if (size < 0)
+			return newLinkedList;
+
+		for (int x = 0; x < size; x++)
+			newLinkedList.AddLast(x);
+
+		foreach (int x in newLinkedList)
+			Console.WriteLine(x);
+
+		return newLinkedList;
+	}
+}
