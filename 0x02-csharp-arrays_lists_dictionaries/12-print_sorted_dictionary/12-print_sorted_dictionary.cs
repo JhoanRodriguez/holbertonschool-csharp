e089900c15178d69e3a52801134d48300438b03e
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-14 21:32:57 -0500

    Adding the 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+	public static void PrintSorted(Dictionary<string, string> myDict)
+	{
+		List<string> keys = new List<string>();
+		foreach (string key in myDict.Keys)
+			keys.Add(key);
+
+		keys.Sort();
+		foreach (string key in keys)
+			Console.WriteLine($"{key}: {myDict[key]}");
+	}
+}
