d63d361e5113a58ae86391527912dc1f619d69a9
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-14 19:10:03 -0500

    Adding the 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+	public static void Reverse(int[] array)
+	{
+		if (array == null)
+			Console.WriteLine();
+		else if (array.Length < 0)
+			Console.WriteLine();
+		else
+		{
+			for (int x = array.Length - 1; x >= 0; x--)
+			{
+				Console.Write(array[x]);
+				if (x != 0)
+					Console.Write(" ");
+			}
+			Console.WriteLine();
+		}
+	}
+
+}
