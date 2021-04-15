69a748b6d60dddfb2e99c08cd8ab3a8f5d7f10a2
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-15 16:02:48 -0500

    Adding the 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+	{
+
+		HashSet<int> hSet1 = new HashSet<int>(list1);
+		HashSet<int> hSet2 = new HashSet<int>(list2);
+
+		hSet1.IntersectWith(hSet2);
+		list1.Clear();
+		foreach (int x in hSet1)
+			list1.Add(x);
+
+		list1.Sort();
+
+		return list1;
+	}
+}
