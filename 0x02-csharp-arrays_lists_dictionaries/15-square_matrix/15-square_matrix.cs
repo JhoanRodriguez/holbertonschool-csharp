0c71f057940469097fc2192b8791f4638437a699
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-14 22:56:06 -0500

    Adding the 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Matrix
+{
+	public static int[,] Square(int[,] myMatrix)
+	{
+		int[,] newMatrix = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+		for (int x = 0; x < myMatrix.GetLength(0); x++)
+		{
+			for (int y = 0; y < myMatrix.GetLength(1); y++)
+			{
+				newMatrix[x, y] = (int)Math.Pow(myMatrix[x, y], 2);
+			}
+		}
+		return newMatrix;
+	}
+}
