b8c14371cabe884d118828f79e53eb548150043f
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-15 21:33:45 -0500

    Adding the 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		LinkedListNode<int> newNode;
+		LinkedListNode<int> tmpNode = new LinkedListNode<int>(n);
+
+		newNode = myLList.First;
+
+		while (newNode != null)
+		{
+			if (newNode.Next.Value < n)
+				newNode = newNode.Next;
+			else
+			{
+				myLList.AddAfter(newNode, tmpNode);
+				break;
+			}
+
+		}
+		return tmpNode;
+	}
+}
