62a5217b721297d7154d130dc3817e9a2488687a
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-15 21:58:07 -0500

    Adding the 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static void Delete(LinkedList<int> myLList, int index)
+	{
+
+		LinkedListNode<int> strider = myLList.First;
+		for (int i = 0; i < index && strider.Next != null; i++)
+			strider = strider.Next;
+		if (i == index)
+			myLList.Remove(strider);
+	}
+}
