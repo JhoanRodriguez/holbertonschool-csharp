6b0c2ec254d09159a7e3774c84cf908a39ceb660
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-15 21:07:48 -0500

    Adding the 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		int index = 0;
+		if (myLList.Count < n)
+			return 0;
+
+		foreach (int num in myLList)
+		{
+			if (index == n)
+				return (num);
+			index++;
+		}
+		return 0;
+	}
+}
