d388b7cd0cc8395ed42cc96f3bb7d168b59b6782
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-15 19:08:16 -0500

    Adding the 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int FindNode(LinkedList<int> myLList, int value)
+	{
+		int counter = 0;
+
+		if (!myLList.Contains(value))
+			return -1;
+
+		foreach (int x in myLList)
+		{
+			if (x == value)
+				break;
+			else
+				counter += 1;
+		}
+		return counter;
+	}
+}
