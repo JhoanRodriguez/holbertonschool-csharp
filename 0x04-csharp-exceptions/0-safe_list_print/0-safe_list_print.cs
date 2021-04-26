a7f0b71c702e74f02aa8d3ddb4bfb0cbc00e4d81
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-04-26 17:50:50 -0500

    Adding the 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+	public static int SafePrint(List<int> myList, int n)
+	{
+		int printed = 0;
+		for (int x = 0; x < n; x++)
+		{
+			try
+			{
+				Console.WriteLine(myList[x]);
+				printed += 1;
+			}
+			catch (System.Exception)
+			{
+
+				break;
+			}
+		}
+		return printed;
+	}
+}
