0b7207f36cab72dfd40c4179718be569582ea34f
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-25 23:45:32 -0500

    Adding the 2-3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary> Class for VectorMath. </summary>
+class VectorMath
+{
+    /// <summary> Calculates and returns the length of a given vector. </summary>
+    public static double Magnitude(double[] vector){
+        if (vector.Length == 1)
+            return -1;
+        double sum = 0f;
+        foreach (var number in vector)
+        {
+            sum += Math.Pow(number,2);
+        }
+        return Math.Round(Math.Sqrt(sum),2);
+    }
+}
