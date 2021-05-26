819e617aab673255f9b5bcbf606c66b67c6ec631
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-26 00:04:34 -0500

    Adding the 9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary> Class for VectorMath. </summary>
+class VectorMath
+{
+	/// <summary> Multiplies a vector and a scalar and returns the resulting vector. </summary>
+	public static double[] Multiply(double[] vector, double scalar)
+	{
+		if (vector.Length == 2)
+		{
+			double[] answer = { 0, 0 };
+			answer[0] = vector[0] * scalar;
+			answer[1] = vector[1] * scalar;
+			return (answer);
+		}
+		else if (vector.Length == 3)
+		{
+			double[] answer = { 0, 0, 0 };
+			answer[0] = vector[0] * scalar;
+			answer[1] = vector[1] * scalar;
+			answer[2] = vector[2] * scalar;
+			return (answer);
+		}
+		else
+		{
+			return (new double[] { -1 });
+		}
+	}
+}
