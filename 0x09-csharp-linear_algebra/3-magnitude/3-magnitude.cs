c9a1413a84f61eda74c8f559f24a283b690f44ac
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-25 23:55:47 -0500

    Fix the 3

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -3,15 +3,16 @@
 /// <summary> Class for VectorMath. </summary>
 class VectorMath
 {
-    /// <summary> Calculates and returns the length of a given vector. </summary>
-    public static double Magnitude(double[] vector){
-        if (vector.Length == 1 || vector.Length > 3)
-            return -1;
-        double sum = 0f;
-        foreach (var number in vector)
-        {
-            sum += Math.Pow(number,2);
-        }
-        return Math.Round(Math.Sqrt(sum),2);
-    }
+	/// <summary> Calculates and returns the length of a given vector. </summary>
+	public static double Magnitude(double[] vector)
+	{
+		if (vector.Length <= 1 || vector.Length > 3)
+			return -1;
+		double sum = 0f;
+		foreach (var number in vector)
+		{
+			sum += Math.Pow(number, 2);
+		}
+		return Math.Round(Math.Sqrt(sum), 2);
+	}
 }
---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class VectorMath
+{
+	public static double[] Add(double[] vector1, double[] vector2)
+	{
+		if (vector1.Length <= 1 || vector1.Length > 3 || vector2.Length <= 1 || vector2.Length > 3)
+			return -1;
+	}
+}
