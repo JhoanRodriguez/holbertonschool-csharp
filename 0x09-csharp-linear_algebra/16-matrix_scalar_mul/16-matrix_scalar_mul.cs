838252b514a431b8decb054bd898559fb70a92d4
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-26 00:26:21 -0500

    Adding 15-16

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary> Class for MatrixMath. </summary>
+class MatrixMath
+{
+	/// <summary> Multiplies a matrix and a scalar and returns the resulting matrix. </summary>
+	public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+	{
+		double[,] empty_matrix = { { -1 } };
+
+		if ((matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) || (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3))
+		{
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					matrix[i, j] = matrix[i, j] * scalar;
+				}
+			}
+			return (matrix);
+		}
+		else
+		{
+			return (empty_matrix);
+		}
+	}
+}
