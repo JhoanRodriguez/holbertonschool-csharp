ef3334fbc469b12f89a737626a6211b2f57e958c
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-25 23:59:44 -0500

    Adding the 6

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿
+using System;
 
+/// <summary> Class for VectorMath. </summary>
 class VectorMath
 {
+	/// <summary> Adds two vectors and returns the resulting vector. </summary>
 	public static double[] Add(double[] vector1, double[] vector2)
 	{
-		if (vector1.Length <= 1 || vector1.Length > 3 || vector2.Length <= 1 || vector2.Length > 3)
-			return -1;
+		if (vector1.Length == 2 && vector2.Length == 2)
+		{
+			double[] answer = { 0, 0 };
+			answer[0] = vector1[0] + vector2[0];
+			answer[1] = vector1[1] + vector2[1];
+			return (answer);
+		}
+		else if (vector1.Length == 3 && vector2.Length == 3)
+		{
+			double[] answer = { 0, 0, 0 };
+			answer[0] = vector1[0] + vector2[0];
+			answer[1] = vector1[1] + vector2[1];
+			answer[2] = vector1[2] + vector2[2];
+			return (answer);
+		}
+		else
+		{
+			return new double[] { -1 };
+		}
 	}
 }
