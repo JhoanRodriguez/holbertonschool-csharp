0ad648b79fc8f3fe692f21ad4caf01b3e594238f
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-26 20:10:40 -0500

    Adding 23 - 24

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary> Class for MatrixMath. </summary>
+class MatrixMath
+{
+	/// <summary> Shears a square 2D matrix by a given shear factor and returns the resulting matrix. </summary>
+	public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+	{
+		double[,] error = { { -1 } }, result = new double[2, 2], shear;
+
+		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+		{
+			if (direction == 'x')
+				shear = new double[,] { { 1, 0 }, { factor, 1 } };
+			else if (direction == 'y')
+				shear = new double[,] { { 1, factor }, { 0, 1 } };
+			else
+				return (error);
+
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					for (int k = 0; k < 2; k++)
+					{
+						result[i, j] += matrix[i, k] * shear[k, j];
+					}
+				}
+			}
+			return (result);
+		}
+		else
+			return (error);
+	}
+}
