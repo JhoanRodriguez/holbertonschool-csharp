3d76eb71f9287b735d8d7e08bd8a741d0c790a1e
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-26 19:26:56 -0500

    Adding 19 - 20

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary> Class for MatrixMath. </summary>
+class MatrixMath
+{
+	/// <summary> Rotates a square 2D matrix by a given angle in radians and returns the resulting matrix. </summary>
+	public static double[,] Rotate2D(double[,] matrix, double angle)
+	{
+		double[,] error = { { -1 } }, result = new double[2, 2];
+		double[,] rotation = { { Math.Cos(angle), Math.Sin(angle) }, { -1 * Math.Sin(angle), Math.Cos(angle) } };
+
+		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					for (int k = 0; k < 2; k++)
+					{
+						result[i, j] = Math.Round((result[i, j] + (matrix[i, k] * rotation[k, j])), 2);
+					}
+				}
+			}
+			return (result);
+		}
+		else
+			return (error);
+	}
+}
