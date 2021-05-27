40b85d73a325ee1841dd90131b8985ff52245d73
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-05-26 19:00:56 -0500

    Adding the 17 - 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary> Class for MatrixMath. </summary>
+class MatrixMath
+{
+	/// <summary> Multiplies two matrices and returns the resulting matrix. </summary>
+	public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+	{
+		double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)], error = { { -1 } };
+		double total;
+
+		if (matrix1.GetLength(1) == matrix2.GetLength(0))
+		{
+			for (int i = 0; i < matrix1.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix2.GetLength(1); j++)
+				{
+					total = 0;
+					for (int k = 0; k < matrix1.GetLength(1); k++)
+					{
+						total += (matrix2[k, j]) * (matrix1[i, k]);
+					}
+					result[i, j] = total;
+				}
+			}
+			return (result);
+		}
+		else
+			return (error);
+	}
+}
