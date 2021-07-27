f89e554b7a93965b6637207e6b8be439cf0abfb6
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-07-27 14:09:31 -0500

    Adding documentation

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 
+/// <summary> Interactive interface </summary>
 interface IInteractive
 {
     void Interact();
 }
 
+/// <summary> Breakable Interface </summary>
 interface IBreakable
 {
    int durability {get; set;}
    void Break();
 }
 
+/// <summary> Collectable Interface </summary>
 interface ICollectable
 {
     bool isCollected {get; set;}
     void Collect();
 }
 
+/// <summary> Class that inherits from Base class and all interfaces </summary>
 class TestObject : IInteractive, IBreakable, ICollectable
 {
     public int durability {get; set;}
